71f68056b8c3855d55c1b73671ff778eb44fc6cf
akashm-25 <akashaadith@gmail.com>
2020-03-27 23:38:35 +0530

    initial commit

---
+++ introduction_to_programming/excercise_38.py
@@ -0,0 +1,19 @@
+side=int(input('Enter the no of sides'))
+if side<3 or side>10:
+	print("Error")
+elif side==3:
+	print('Triangle')
+elif side==4:
+	print('square')
+elif side==5:
+	print('pentagon')
+elif side==6:
+	print('Hexagon')
+elif side==7:
+	print('heptagon')
+elif side==8:
+	print('octagon')
+elif side==9:
+	print('nonagon')
+elif side==10:
+	print('decagon')
